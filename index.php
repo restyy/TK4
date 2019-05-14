1b85055936cdebdc3c1c2722aff3bdeee65703a4
dian9395 <dian9395@gmai.com>
2019-05-14 15:48:36 +0700

    add new file

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+<html>
+<head>
+    <title>Depok Bike Sharing</title>
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+	<script src="js/jquery-2.1.1.min.js" type="text/javascript"></script>
+</head>
+<body>
+      	
+			
+				
+	<section id="content">
+        <div class="bg-top">
+        	<div class="bg-top-2">
+                    <div class="bg-top-shadow">
+                        <div class="main">
+                            <div class="box">
+                                <div class="padding">
+                                    <div class="container_12">
+                                        <div class="wrapper">
+                                            <div class="grid_12">
+                                            	<div class="indent-left">
+                                                	                                               
+<table>
+	<form name="register" id="contact-form" method="post" action="" enctype="multipart/form-data" onSubmit="return valregister()">		
+	<fieldset><br></br>
+<h4><a href="index.html"><center><img src="images/Depok Bike Sharing.jpg" width=620px height=207px> </center></a></h4> 
+										<center><tr>
+										<td cols="3"> <input class="button" type="submit" name="tombol" value="LOGIN" a href="admin.tml"></a></td>
+										<td cols="3"> <input class="button" type="submit" name="tombol" value="SIGN UP"></a></td>
+										</tr></center>
+								
+
+
+</fieldset>	
+</form>
+
+</table>                        
+                                                       				
+                                                 
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+			</div>	
+        </div>
+ 
+    </section>
+    <footer>
+        <div class="main">
+        	<div class="container_12">
+            	<div class="wrapper">
+                	<div class="grid_4">
+                    	<div>Copyright &copy; 2019 <a class="link color-3">Kelompok 1 - Matrikulasi Basdat</a></div>
+                    
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+   
+</body>
+
+</html>
+
+?>
